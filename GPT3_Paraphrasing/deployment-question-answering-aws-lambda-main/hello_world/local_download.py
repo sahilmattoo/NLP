822204bdeeb7fcce88d79358177b113b1a5307ff
Sahil Mattoo <42411417+sahilmattoo@users.noreply.github.com>
2025-03-29 01:59:10 +0530

    Delete GPT3_Paraphrasing directory

--- GPT3_Paraphrasing/deployment-question-answering-aws-lambda-main/hello_world/local_download.py
+++
@@ -1,8 +0,0 @@
-from transformers import AutoTokenizer, AutoModelForQuestionAnswering
-
-tokenizer = AutoTokenizer.from_pretrained("deepset/roberta-base-squad2")
-model = AutoModelForQuestionAnswering.from_pretrained("deepset/roberta-base-squad2")
-
-
-model.save_pretrained('./roberta-base-squad2')
-tokenizer.save_pretrained('./roberta-base-squad2')
